6bfa8169e26190353755a159d5e75f2c63845084
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-01-25 13:23:59 -0600

    Wrote basic enemy ai script

---
+++ Assets/Scripts/Car/CarControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControl : MonoBehaviour {
+
+	public float rotationSpeed = 1.0f;
+	public float moveSpeed     = 1.0f;
+
+	private Rigidbody body;
+	private Vector3 direction;
+
+	void Start(){
+		body = gameObject.GetComponent<Rigidbody>();
+	}
+
+	void OnDrive(Vector3 dir){
+		direction = dir;
+	}
+
+	void FixedUpdate(){
+		body.MoveRotation(Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime));
+
+		if(direction.magnitude > 0){
+			Vector3 moveVector = transform.forward * moveSpeed * Time.deltaTime;
+			body.MovePosition(transform.position + moveVector);
+		}
+	}
+}
