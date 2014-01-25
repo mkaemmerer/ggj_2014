da067358b9c08c035e9576bb7a049922b7db619e
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-01-25 16:39:00 -0600

    Added enemy car to silent film track and created checkpoints

---
+++ Assets/Scripts/Game Events/MoveToChannel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToChannel : MonoBehaviour {
+	
+	void OnChannelChange (LayerMask newChannel) {
+		gameObject.layer = newChannel;
+	}
+}
