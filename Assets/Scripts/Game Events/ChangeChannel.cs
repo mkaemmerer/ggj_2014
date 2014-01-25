d708de4391c06a4e9857f74fa479cb314f253459
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-01-25 13:58:38 -0600

    Added some simple channel-switching scripts

---
+++ Assets/Scripts/Game Events/ChangeChannel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeChannel : MonoBehaviour {
+
+	private ChannelManager manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.FindObjectOfType<ChannelManager>() as ChannelManager;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			manager.ChangeChannel();
+		}
+	}
+}
---
+++ Assets/Scripts/Game Events/ShowChannel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowChannel : MonoBehaviour {
+
+	private Camera cam;
+
+	void Start(){
+		cam = gameObject.GetComponent<Camera>();
+	}
+
+	void OnChannelChange (LayerMask layer) {
+		cam.cullingMask = 1 << layer;
+		Debug.Log(cam.cullingMask);
+	}
+	void OnGhostChannelChange (LayerMask layer) {
+		cam.cullingMask = cam.cullingMask | (1 << layer);
+	}
+}
