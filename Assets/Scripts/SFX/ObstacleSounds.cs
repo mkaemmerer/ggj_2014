e242561a458600d3d5a096eaba9fccedb73f1b37
broccoliDespot <vaughan.ta@gmail.com>
2014-01-26 11:35:09 -0800

    Added sounds to obstacle prefabs

--- Assets/Scripts/SFX/ObstacleSounds.cs
+++ Assets/Scripts/SFX/ObstacleSounds.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class ObstacleSounds : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public AudioSource output;
+	public float collisionSoundLength = 1.0f;
+
+	private float collisionTime = 0.0f;
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (collisionTime != 0.0f) {
+			collisionTime = Mathf.Max (0.0f, collisionTime - Time.deltaTime);
+		} else if (output.isPlaying) {
+			output.Stop();
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+		output.time = Random.Range (0.0f, 1.0f);
+		output.Play ();
+		collisionTime = collisionSoundLength;
 	}
 }
