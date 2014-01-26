f38608d5e6311a00faee2de3f75e97cfd4e89da2
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-01-26 13:46:34 -0600

    Wrote script to defeat villain and end the game

---
+++ Assets/Scripts/Game Events/EndGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+	public float sloMoRate = 0.1f;
+	private bool gameHasEnded = false;
+	private CarControl[] cars;
+
+	void OnGameEnd(){
+		gameHasEnded = true;
+		StartCoroutine(End());
+	}
+
+	void Start(){
+		cars = GameObject.FindObjectsOfType<CarControl>() as CarControl[];
+	}
+
+	void Update(){
+		if(gameHasEnded){
+			foreach(CarControl car in cars){
+				car.moveSpeed *= 1.0f - ((1.0f - sloMoRate) * Time.deltaTime);
+			}
+		}
+	}
+
+	IEnumerator End(){
+		yield return new WaitForSeconds(3.0f);
+		Debug.Log("Go To Next Scene");
+		Application.LoadLevel("EndScreen");
+	}
+}
--- Assets/Scripts/Game Events/StealRemote.cs
+++ Assets/Scripts/Game Events/StealRemote.cs
@@ -6,15 +6,27 @@ public class StealRemote : MonoBehaviour {
 	private bool hasRemote = false;
 
 	void OnCollisionEnter(Collision collision){
-		if(hasRemote) return;
-
-		EnemyUseRemote enemy = collision.gameObject.GetComponent<EnemyUseRemote>();
-		if(enemy != null && enemy.hasRemote){
-			enemy.SendMessage("OnStealRemote");
-			hasRemote = true;
-
-			//Now we can change the channel
-			gameObject.AddComponent("ChangeChannel");
+		if(collision.gameObject.CompareTag("Enemy")){
+			//If we don't have the remote, try to steal it back
+			if(!hasRemote){
+				Steal(collision.gameObject);
+			}
+			//Otherwise, defeat the enemy!
+			else {
+				Defeat (collision.gameObject);
+			}
 		}
+	}
+
+	void Steal(GameObject enemy){
+		EnemyUseRemote enemyRemote = enemy.GetComponent<EnemyUseRemote>();
+		enemyRemote.SendMessage("OnStealRemote");
+		hasRemote = true;
+		//Now we can change the channel
+		gameObject.AddComponent("ChangeChannel");
+	}
+
+	void Defeat(GameObject enemy){
+		gameObject.SendMessageUpwards("OnGameEnd");
 	}
 }
