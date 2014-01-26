699333333a1de076d76ed9990b0b42e04b36343a
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-01-25 23:05:26 -0600

    Imported silent film skybox assets. Added steal remote script.

---
+++ Assets/Scripts/Game Events/StealRemote.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StealRemote : MonoBehaviour {
+	
+	private bool hasRemote = false;
+
+	void OnCollisionEnter(Collision collision){
+		if(hasRemote) return;
+
+		EnemyUseRemote enemy = collision.gameObject.GetComponent<EnemyUseRemote>();
+		if(enemy != null && enemy.hasRemote){
+			enemy.SendMessage("OnStealRemote");
+			hasRemote = true;
+
+			//Now we can change the channel
+			gameObject.AddComponent("ChangeChannel");
+		}
+	}
+}
