62ef8c0ca723b3371c183320dc002b7b85690434
broccoliDespot <vaughan.ta@gmail.com>
2014-01-26 10:40:31 -0800

    CarSounds script and sounds

--- Assets/Scripts/SFX/CarSounds.cs
+++ Assets/Scripts/SFX/CarSounds.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class CarSounds : MonoBehaviour {
 
+	public AudioSource engineOutput;
+	public AudioSource tireOutput;
+	public float squealMin;
+	public float squealMax;
+
+	private float squealTime = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+		engineOutput.Play ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update() {
+		if (squealTime != 0.0f) {
+			squealTime = Mathf.Max (0.0f, squealTime - Time.deltaTime);
+		} else if (tireOutput.isPlaying) {
+			tireOutput.Stop();
+		}
+	}
+
+	void OnSharpTurn()
+	{
+		tireOutput.time = Random.Range (0.0f, tireOutput.clip.length);
+		tireOutput.Play();
+		squealTime = Random.Range (squealMin, squealMax);
 	}
 }
